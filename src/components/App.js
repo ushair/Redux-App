dee753761c964a7eaf66ccec8ba151a359e22c79
ushair <ushairansari@gmail.com>
2021-04-11 23:15:56 +0530

    1

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home'
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
